9ac231a46fc0aa4d0651243ffe7f02efb6935188
Dylan Turner <dylan.turner22@gmail.com>
2013-01-23 15:24:45 -0700

    added the first function to search for artist by city

--- js/script.js
+++ js/script.js
@@ -35,7 +35,6 @@ $(document).ready(function () {
 				getArtistGigographyAndCallVenueInfo(artistId);
 			});
 
-		return artistId;
 
 	};
 
@@ -113,8 +112,62 @@ $(document).ready(function () {
 
 	};
 
+	// Code to get venues of a city  for a band
+	
+	var getArtistIDAndCallGig = function (city, artist) {
+		var artistId;
+		$.getJSON("http://api.songkick.com/api/3.0/search/artists.json?query=" + artist + "&page=1&apikey=vDtvjogcJwz6gi6J&jsoncallback=?", function(data){
+				artistId = data.resultsPage.results.artist[0].id;
+				
+				getArtistGigographyAndCallVenue(city, artistId);
+				
+			});
+
+
+	};
+	
+	var	getArtistGigographyAndCallVenue = function (city, artistId) {
+		$.getJSON("http://api.songkick.com/api/3.0/artists/" + artistId + "/gigography.json?apikey=vDtvjogcJwz6gi6J&jsoncallback=?", function(data){
+
+			if(data.resultsPage.results.event) {
+				for (var i = 0; i < data.resultsPage.results.event.length; i++) {
+				fromIdGetVenueIn(city, data.resultsPage.results.event[i].venue.id);
+				
+				}
+			} else {
+				console.log("nothing was returned");
+			}
+
+		});
+
+	};
+
+	var fromIdGetVenueIn = function (city, venueId) {
+		if(venueId) {
+			$.getJSON("http://api.songkick.com/api/3.0/venues/" + venueId + ".json?apikey=vDtvjogcJwz6gi6J&jsoncallback=?", function(data){
+
+				var venueObject = data.resultsPage.results.venue;
+				if(venueObject.city.displayName.toLowerCase() === city) {
+					$("#container").append("<div><a target='_blank' href=" + $.trim(venueObject.website) + ">" + venueObject.website + "</a></div>");
+				}
+				
+
+			});
+		} else {
+			console.log("Returned null");
+		}
+
+
+		$("#hidden").addClass("hide");
+
+	};
+
+
+	// End of code to get venues of a city for a band
+
 	var getWebsitesOfArtistForCity = function (city, artist) {
-		alert("Having a city and band name doesn't work quite yet. choose one or the other");
+		// alert("Having a city and band name doesn't work quite yet. choose one or the other");
+		getArtistIDAndCallGig(city, artist);
 	};
 
 	$("#search").on("click", search);
