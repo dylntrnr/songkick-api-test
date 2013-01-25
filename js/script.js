ecf0911f38a044073ddb82fd50ccbd85232b5ffc
Dylan Turner <dylan.turner22@gmail.com>
2013-01-25 12:35:35 -0700

    adjusted template, added page button

--- js/script.js
+++ js/script.js
@@ -5,21 +5,39 @@ $(document).ready(function () {
 	var VenuesObject = Parse.Object.extend("VenuesObject");
 	var venuesObject = new VenuesObject();
 
-	var search = function () {
+	var search = function (pageNumber) {
 		$("#container").html("");
-		$("#hidden").removeClass("hide");
 		var city = $("#term").val();
 		var artist = $("#term2").val();
 
-		if(artist && city) {
-			getWebsitesOfArtistForCity(city, artist);
-		} else if(artist && !city) {
-			getArtistIDAndCallGigo(artist);
-		} else if(city) {
-			getWebsites(city);
-			
+		if(!pageNumber) {
+			if(artist && city) {
+				getWebsitesOfArtistForCity(city, artist);
+				$("#hidden").removeClass("hide");
+			} else if(artist && !city) {
+				getArtistIDAndCallGigo(artist);
+				$("#hidden").removeClass("hide");
+			} else if(city) {
+				getWebsites(city);
+				$("#hidden").removeClass("hide");
+				
+			} else {
+				alert("Please enter a city like: san diego or artist like: larusso");
+			}
 		} else {
-			alert("Please enter a city like: san diego or artist like: larusso");
+			if(artist && city) {
+				getWebsitesOfArtistForCity(city, artist);
+				$("#hidden").removeClass("hide");
+			} else if(artist && !city) {
+				getArtistIDAndCallGigo(artist);
+				$("#hidden").removeClass("hide");
+			} else if(city) {
+				getWebsites(city, pageNumber);
+				$("#hidden").removeClass("hide");
+				
+			} else {
+				alert("Please enter a city like: san diego or artist like: larusso");
+			}
 		}
 
 	};
@@ -37,9 +55,8 @@ $(document).ready(function () {
 	
 
 	var getWebsites = function  (city, pageNumber) {
-
+		$("#pageBtn").removeClass("hide");
 		var template = $("#testTpl").html();
-		$("#page").html(pageNumber);
 
 		if(!pageNumber) {
 			pageNumber = 1;
@@ -53,9 +70,9 @@ $(document).ready(function () {
 				var html = Mustache.to_html(template, data.resultsPage.results);
 				$("#container").html(html);
 
+				$("#hidden").addClass("hide");
 		});
 	
-		$("#hidden").addClass("hide");
 		
 
 	};
@@ -78,17 +95,15 @@ $(document).ready(function () {
 
 
 	var fromIdGetVenueInfo = function (venueId) {
+		
+		var template = $("#testTpl").html();
+
 		if(venueId) {
 			$.getJSON("http://api.songkick.com/api/3.0/venues/" + venueId + ".json?apikey=vDtvjogcJwz6gi6J&jsoncallback=?", function(data){
 
-				var venueObject = data.resultsPage.results.venue;
-				if(venueObject.website) {
-					$("#container").append("<div><a target='_blank' href=" + $.trim(venueObject.website) + ">" + venueObject.website + "</a></div>");
-				}
-				if(venueObject.description) {
-						$("#container").append("<div><strong>" + venueObject.description + "</strong></div>");
-					}
-
+				var html = Mustache.to_html(template, data.resultsPage.results);
+				$("#container").append(html);
+				
 			});
 		} else {
 			console.log("Returned null");
@@ -130,15 +145,17 @@ $(document).ready(function () {
 	};
 
 	var fromIdGetVenueIn = function (city, venueId) {
+		var template = $("#testTpl").html();
 		if(venueId) {
 			$.getJSON("http://api.songkick.com/api/3.0/venues/" + venueId + ".json?apikey=vDtvjogcJwz6gi6J&jsoncallback=?", function(data){
 
-				var venueObject = data.resultsPage.results.venue;
-				if(venueObject.city.displayName.toLowerCase() === city) {
-					$("#container").append("<div><a target='_blank' href=" + $.trim(venueObject.website) + ">" + venueObject.website + "</a></div>");
+				if(data.resultsPage.results.venue.city.displayName.toLowerCase() === city) {
+					
+					var html = Mustache.to_html(template, data.resultsPage.results);
+					$("#container").append(html);
 				}
 				
-				venuesObject.save("website", venueObject.website);
+				// venuesObject.save("website", venueObject.website);
 
 			});
 		} else {
@@ -168,6 +185,12 @@ $(document).ready(function () {
 		if(event.keyCode == 13){
 			search();
 		}
+	});
+
+	var counter = 2;
+	$("#pageBtn").on("click", function(event) {
+		search(counter);
+		counter += 1;
 	});
 
 	
